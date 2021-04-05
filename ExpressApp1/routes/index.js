053a643d8c3d019edf0e7d02fd36680d9df04a8d
phaantoom <34126232+phaantoom@users.noreply.github.com>
2021-04-06 00:30:21 +0200

    Update index.js

--- ExpressApp1/routes/index.js
+++ ExpressApp1/routes/index.js
@@ -1,7 +1,6 @@
 'use strict';
 var express = require('express');
 var router = express.Router();
-
 const db = require("../db/db.js");
 
 /* GET home page. */
@@ -13,7 +12,8 @@ router.get('/', function (req, res) {
 router.post('/matchEvent/:Venue/:day/:month/:year/:hour/:minut', (req, res) => {//---------------------------------------------post
     //STR_TO_DATE(req.params.day + "/" + req.params.month + "/" + req.params.year + " " + req.params.hour + ":" + req.params.minut + ":00", '%d/%m/%Y %H:%i:%s');
     //let de = req.params.day + "/" + req.params.month + "/" + req.params.year + " " + req.params.hour + ":" + req.params.minut + ":00";
-    const event = new Date(req.params.year, req.params.month, req.params.day, req.params.hour, req.params.minut, 7);
+    const event = new Date(req.params.year, req.params.month -1, req.params.day, req.params.hour, req.params.minut, 7);
+    console.log(req.params.month);
     console.log(event.toString());
     console.log(event.toDateString());
     //console.log(de);
@@ -22,9 +22,29 @@ router.post('/matchEvent/:Venue/:day/:month/:year/:hour/:minut', (req, res) => {
     let query = db.query(sql, match, (err, result) => {
         if (err) throw err;
         console.log(result.insertId);
-        res.send(result);
+        var id = { ID: result.insertId}
+        res.send(id);
     });
 });
+
+
+
+//router.post('/matchEvent', (req, res) => {//---------------------------------------------post
+//    //STR_TO_DATE(req.params.day + "/" + req.params.month + "/" + req.params.year + " " + req.params.hour + ":" + req.params.minut + ":00", '%d/%m/%Y %H:%i:%s');
+//    //let de = req.params.day + "/" + req.params.month + "/" + req.params.year + " " + req.params.hour + ":" + req.params.minut + ":00";
+//    console(req.body);
+//    const event = new Date(req.body.year, req.body.month, req.body.day, req.body.hour, req.body.minut, 7);
+//    console.log(event.toString());
+//    console.log(event.toDateString());
+//    //console.log(de);
+//    let match = [req.body.Venue, event];
+//    let sql = 'INSERT INTO EPL_match (venue_id,match_date) values (?,?)';
+//    let query = db.query(sql, match, (err, result) => {
+//        if (err) throw err;
+//        console.log(result.insertId);
+//        res.send(result);
+//    });
+//});
 
 router.put('/updatematch/:id/:Venue/:day/:month/:year/:hour/:minut', (req, res) => {//---------------------------------------------put
     //let match = [req.params.Venue, req.params.year + "-" + req.params.month + "-" + req.params.day + " " + req.params.hour + ":" + req.params.minut + ":00", req.params.id];
@@ -47,6 +67,7 @@ router.post('/matchEventTeamsB/:id/:Team1/:state1/:Team2/:state2', (req, res) =>
         [req.params.Team1, req.params.id, req.params.state1],
         [req.params.Team2, req.params.id, req.params.state2]
     ];
+    console.log(match_team);
     let sql = 'INSERT INTO match_team (team, match_id, state) values ?';
     let query = db.query(sql, [match_team], (err, result) => {
         if (err) throw err;
@@ -55,19 +76,19 @@ router.post('/matchEventTeamsB/:id/:Team1/:state1/:Team2/:state2', (req, res) =>
     });
 });
 
-//router.put('/updatematchTeamsB/:id/:Team1/:state1/:Team2/:state2', (req, res) => {//---------------------------------------------put
+router.put('/updatematchTeamsB/:id/:Team1/:state1/:Team2/:state2', (req, res) => {//---------------------------------------------put
 
-//    let match_team = [
-//        [req.params.Team1, req.params.state1, req.params.id],
-//        [req.params.Team2, req.params.state2, req.params.id]
-//    ];
-//    let sql = 'UPDATE match_team SET team = ?, state = ? WHERE match_id = ?';
-//    let query = db.query(sql, match_team, (err, result) => {
-//        if (err) throw err;
-//        console.log(result.insertId);
-//        res.send(result);
-//    });
-//});
+    let match_team = [
+        [req.params.Team1, req.params.state1, req.params.id],
+        [req.params.Team2, req.params.state2, req.params.id]
+    ];
+    let sql = 'UPDATE match_team SET team = ?, state = ? WHERE match_id = ?';
+    let query = db.query(sql, match_team, (err, result) => {
+        if (err) throw err;
+        console.log(result.insertId);
+        res.send(result);
+    });
+});
 
 router.put('/updatematchTeamsB/:id/:Team1/:state1/:oldTeam', (req, res) => {//---------------------------------------------put
 
@@ -94,15 +115,13 @@ router.post('/matchEventTeams/:Team/:state/:id', (req, res) => {      //--------
     });
 });
 
-router.put('/matchEventrefeB/:ref/:id/:line1/:line2', (req, res) => {//---------------------------------------------put
+router.put('/matchEventrefeB/:ref/:id/:oldref', (req, res) => {//---------------------------------------------put
 
     let match_refe = [
-        [req.params.ref, req.params.id],
-        [req.params.line1, req.params.id],
-        [req.params.line2, req.params.id]
+        req.params.ref, req.params.id, req.params.oldref
     ];
-    let sql = 'UPDATE match_team SET ref = ? WHERE match_id = ?';
-    let query = db.query(sql, [match_refe], (err, result) => {
+    let sql = 'UPDATE refe SET ref = ? WHERE match_id = ? and ref = ?';
+    let query = db.query(sql, match_refe, (err, result) => {
         if (err) throw err;
         console.log(result.insertId);
         res.send(result);
@@ -248,6 +267,116 @@ router.delete('/deleteuser/:id', (req, res) => {               //---------------
     });
 });
 
+
+router.post('/registeruser/:user_name/:user_type/:first_name/:last_name/:pass/:mail/:gender/:address/:city/:day/:month/:year', (req, res) => {
+    //let user_info = {
+    //    user_name: req.params.user_name, user_type: req.params.user_type, first_name: req.params.first_name,
+    //    last_name: req.params.last_name, pass: req.params.pass, mail: req.params.mail, gender: req.params.gender,
+    //    address: req.params.address, city: req.params.city, birth: req.params.year + "-" + req.params.month + "-" + req.params.day
+    //};
+    let user_info = [
+        req.params.user_name, req.params.user_type, req.params.first_name,
+        req.params.last_name, req.params.pass, req.params.mail, req.params.gender,
+        req.params.address, req.params.city, req.params.year + "-" + req.params.month + "-" + req.params.day
+    ];
+    let sql = 'insert into app_user (user_name,user_type,first_name,last_name,pass,mail,gender,address,city,birth) values (?,?,?,?,?,?,?,?,?,?)';
+    let query = db.query(sql, user_info, (err, result) => {
+        if (err) throw err;
+        console.log(result.insertId);
+        res.send(result);
+    });
+});
+
+router.post('/getuser/:user_name/:pass', (req, res) => {
+    let credentials = [req.params.user_name, req.params.pass];
+    let sql = 'select* from app_user where user_name = ? and pass = ?';
+    console.log(sql, credentials);
+    let query = db.query(sql, credentials, (err, result) => {
+        if (err) throw err;
+        console.log(result.insertId);
+        res.send(result);
+    });
+});
+
+
+//router.get('/getmatchdeets/:match_id', (req, res) => {
+//    // m match / r ref<->match / re ref / v venue / mt match<->team / t team
+//    //select* can be changed to specific requirements
+//    let sql = 'SELECT* from EPL_match m ON m.ID = ? INNER JOIN refe r ON m.ID = r.match_id INNER JOIN referee re ON r.ref = re.ID INNER JOIN venue v ON m.ID = v.ID INNER JOIN match_team mt ON m.ID = mt.match_id INNER JOIN team t ON mt.team =t.ID';
+//    let query = db.query(sql, req.params.match_id, (err, result) => {
+//        if (err) throw err;
+//        console.log(result.insertId);
+//        res.send(result);
+//    });
+//});
+
+router.get('/getmatchdeets/:match_id', (req, res) => {
+    // m match / r ref<->match / re ref / v venue / mt match<->team / t team
+    //select* can be changed to specific requirements
+    let sql = 'SELECT * from EPL_match where ID = ?';
+    let query = db.query(sql, req.params.match_id, (err, result) => {
+        if (err) throw err;
+        console.log(result.insertId);
+        res.send(result);
+    });
+});
+
+
+router.put('/updateuser/:first_name/:last_name/:pass/:gender/:address/:city/:day/:month/:year/:user_name', (req, res) => {
+    let updated_info = [req.params.first_name, req.params.last_name, req.params.pass, req.params.gender, req.params.address, req.params.city, req.params.year + "-" + req.params.month + "-" + req.params.day, req.params.user_name];
+    let sql = 'UPDATE app_user SET first_name = ?, last_name = ?, pass = ?, gender = ?, address = ? , city = ?, birth = ? WHERE user_name = ?';
+    let query = db.query(sql, updated_info, (err, result) => {
+        if (err) throw err;
+        console.log(result.insertId);
+        res.send(result);
+    });
+});
+
+//reserve ticket given match the IDs of match seat and user
+router.post('/reserveticket/:match_id/:seat_id/:user_id', (req, res) => {
+    let data = [req.params.match_id, req.params.seat_id, req.params.user_id];
+    let sql = 'insert into match_seats (match_ID,seat_id,user_id) values (?,?,?)';
+    let query = db.query(sql, data, (err, result) => {
+        if (err) throw err;
+        console.log(result.insertId);
+        res.send(result);
+    });
+});
+
+
+//delete a reservation given time before match > 3 days 
+router.delete('/deletereservation/:seat_id/:user_name', (req, res) => {               //---------------------------------------------delete
+
+    //let venue_info = { location: req.params.loc, shape: req.params.shape, no_seats: req.params.seats };
+    let data = [req.params.seat_id, req.params.user_name];
+    let sql = 'delete from match_seats where seat_id =? and user_id = ?';
+    let query = db.query(sql, data, (err, result) => {
+        if (err) throw err;
+        console.log(result.insertId);
+        res.send(result);
+    });
+});
+
+//add new ref by type and name
+router.post('/addref/:referee_type/:first_name', (req, res) => {
+    let data = [req.params.referee_type, req.params.first_name];
+    let sql = 'insert into referee (referee_type,first_name) values (?,?)';
+    let query = db.query(sql, data, (err, result) => {
+        if (err) throw err;
+        console.log(result.insertId);
+        res.send(result);
+    });
+});
+
+router.post('/addteam/:team_name/:team_town', (req, res) => {
+    let data = [req.params.team_name, req.params.team_town];
+    let sql = 'insert into team (team_name,team_town) values (?,?)';
+    let query = db.query(sql, data, (err, result) => {
+        if (err) throw err;
+        console.log(result.insertId);
+        res.send(result);
+    });
+});
 
 
 module.exports = router;
